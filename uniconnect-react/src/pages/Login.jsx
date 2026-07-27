41f19063c06ed01ee6c8ee9cd2ac0db77b3cf994
age30s <adithprasad@gmail.com>
2026-07-27 13:07:31 -0500

    changed comp struct

---
+++ uniconnect-react/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+
+export default function Login() {
+//   const handleSubmit = () => {
+//     alert('Message sent!');
+//   };
+
+  return (
+    <div class="grid grid-cols-3 min-h-screen w-full">
+        <div class="col-span-1 bg-blue-500 p-6">
+            <p>Left Side Content (1/3)</p>
+        </div>
+    
+        <div class="col-span-2 bg-red-500 p-6">
+            <p>Right Side Content (2/3)</p>
+        </div>
+    </div>
+
+  );
+}
